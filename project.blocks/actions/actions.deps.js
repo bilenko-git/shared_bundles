3f46cd35d5d06c9d0815c4a8900c08fa349435fc
Vasiliy Polyn <Vasiliy.Polyn@life.com.by>
2017-02-23 10:02:31 +0300

    main-user modal fix #1

--- project.blocks/actions/actions.deps.js
+++ project.blocks/actions/actions.deps.js
@@ -3,7 +3,8 @@
         { block: 'modal', tech: 'bemhtml' },
         { block: 'modal', mods: { theme: 'islands', autoclosable: true } },
         { block: 'modal', mods: { theme: 'life-light' }, tech: 'css' },
-        { block: 'modal', mods: { theme: 'islands' }, tech: 'css' }
+        { block: 'modal', mods: { theme: 'islands' }, tech: 'css' },
+        // { block: 'i-bem' }
     ],
     shouldDeps: [
         { elem: 'inner', techs: 'css' }
--- project.blocks/actions/actions.js
+++ project.blocks/actions/actions.js
@@ -1,26 +1,38 @@
-modules.define('actions', ['i-bem-dom', 'modal', 'BEMHTML', 'jquery'], function(provide, bemDom, Modal, bemHtml, $) {
+modules.define('actions', ['i-bem-dom'/*,'i-bem'*/, 'modal', 'BEMHTML', 'jquery'], function(provide, bemDom/*,iBem*/, Modal, bemHtml, $) {
 
     provide(bemDom.declBlock(this.name, {
         onSetMod: {
             js: {
                 inited: function() {
+                    // console.log(Modal);
+                    // console.log(iBem);
+                    // console.log(modules.getState('modal'));
                     this._domEvents('button').on('click', function() {
-                        debugger;
-
-                        // console.log(Modal);
-                        console.log(this.findChildBlock(Modal) === null);
-                        if( this.findChildBlock(Modal) === null) {
+                        // debugger;
+                        // act = this;
+                        // console.log(this.findChildElem('modal'));
+                        // console.log(this.findParentBlock(Modal));
+                        // console.log(this.findChildBlock(Modal) === null);
+                        console.log(this.domElem);
+                        if( !this._modalWindow) {
                             var modal = bemHtml.apply({
                                 block: 'modal',
+                                // mix: { block: 'actions', js: { id: act.params.id } },
                                 mods : { visible: false, theme : 'life-light', autoclosable: true },
-                                content: 'test',
+                                content: {
+                                    block: 'button',
+                                    text: 'Закрыть'
+                                },
                             });
-                            var mod = bemDom.append(this.domElem, modal);
+                            this._modalWindow = bemDom.append(this.findChildElem('inner').domElem, modal);
                         }
+                        this._modalWindow.bem(Modal).setMod('visible');
+                        // Modal(mod)
                         // console.log(this.findChildBlock(Modal));
 
-                        var modalWindow = this.findChildBlock(Modal);
-                        modalWindow.toggleMod('visible');
+                        // var modalWindow = this.findChildBlock(Modal);
+                        // modalWindow.toggleMod('visible');
+                        // modalWindow.toggleMod('visible');
                         debugger;
                     });
                 }
