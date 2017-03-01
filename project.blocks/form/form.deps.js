a3afe4fd1c2fbe5459fe4121110997618beaa660
Vasiliy Polyn <Vasiliy.Polyn@life.com.by>
2017-03-01 14:45:18 +0300

    deps for form

--- project.blocks/form/form.deps.js
+++ project.blocks/form/form.deps.js
@@ -1,4 +1,4 @@
-({
+([{
     tech: 'js',
     mustDeps: [
         { block: 'dropdown', tech: 'bemhtml' },
@@ -9,6 +9,9 @@
     mustDeps: [
         { block: 'button' },
         { block: 'button', mods: { view: 'main' } },
+        { block: 'input', mods: { theme: 'life-light' } },
+        { block: 'button', mods: { theme: 'life-light' } },
+        { block: 'select', mods: { theme: 'life-light' } },
     ],
     shouldDeps: [
         // { block: 'dropdown', tech: 'bemhtml' },
@@ -29,11 +32,9 @@
         { elem: 'submit' },
         { elem: 'text-error' },
         { elem: 'head-title' },
-        { elem: '<test-button></test-button>s' },
-        { block: 'input' },
         { block: 'checkbox' },
         { block: 'menu', mods: { theme: 'islands' }, tech: 'css' },
         { block: 'select', mods: { theme: 'islands' }, tech: 'css' },
         { block: 'input', mods: { theme: 'islands' }, tech: 'css' }
     ]
-})
+}])
