2279e45d3ac5cd4b307f23fd91fcdd896edc2a9e
Vasiliy Polyn <Vasiliy.Polyn@life.com.by>
2017-03-01 14:48:17 +0300

    form deps

--- project.blocks/form/form.deps.js
+++ project.blocks/form/form.deps.js
@@ -1,40 +1,42 @@
-([{
-    tech: 'js',
-    mustDeps: [
-        { block: 'dropdown', tech: 'bemhtml' },
-        // { block: 'popup', tech: 'bemhtml' },
-        { block: 'tooltip', tech: 'bemhtml' }
-    ]
-},{
-    mustDeps: [
-        { block: 'button' },
-        { block: 'button', mods: { view: 'main' } },
-        { block: 'input', mods: { theme: 'life-light' } },
-        { block: 'button', mods: { theme: 'life-light' } },
-        { block: 'select', mods: { theme: 'life-light' } },
-    ],
-    shouldDeps: [
-        // { block: 'dropdown', tech: 'bemhtml' },
-        { block: 'dropdown' },
-        { block: 'form', tech: 'bemhtml' },
+([
+    {
+        tech: 'js',
+        mustDeps: [
+            { block: 'dropdown', tech: 'bemhtml' },
+            // { block: 'popup', tech: 'bemhtml' },
+            { block: 'tooltip', tech: 'bemhtml' }
+        ]
+    },{
+        mustDeps: [
+            { block: 'button' },
+            { block: 'button', mods: { view: 'main' } },
+            { block: 'input', mods: { theme: 'life-light' } },
+            { block: 'button', mods: { theme: 'life-light' } },
+            { block: 'select', mods: { theme: 'life-light' } },
+        ],
+        shouldDeps: [
+            // { block: 'dropdown', tech: 'bemhtml' },
+            { block: 'dropdown' },
+            { block: 'form', tech: 'bemhtml' },
 
-        // { block: 'button', tech: 'bemhtml' },
-        { block: 'link', tech: 'bemhtml' },
-        { block: 'select', tech: 'bemhtml' },
-        { block: 'tooltip', tech: 'css' },
-        { block: 'select', mods: { mode: 'radio' }, tech: 'bemhtml' },
-        { block: 'modal', mods: { autoclosable: true } },
-        { block: 'popup', mods: { autoclosable: true } },
-        { elem: 'button-group' },
-        { elem: 'title' },
-        { elem: 'text' },
-        { elem: 'label' },
-        { elem: 'submit' },
-        { elem: 'text-error' },
-        { elem: 'head-title' },
-        { block: 'checkbox' },
-        { block: 'menu', mods: { theme: 'islands' }, tech: 'css' },
-        { block: 'select', mods: { theme: 'islands' }, tech: 'css' },
-        { block: 'input', mods: { theme: 'islands' }, tech: 'css' }
-    ]
-}])
+            // { block: 'button', tech: 'bemhtml' },
+            { block: 'link', tech: 'bemhtml' },
+            { block: 'select', tech: 'bemhtml' },
+            { block: 'tooltip', tech: 'css' },
+            { block: 'select', mods: { mode: 'radio' }, tech: 'bemhtml' },
+            { block: 'modal', mods: { autoclosable: true } },
+            { block: 'popup', mods: { autoclosable: true } },
+            { elem: 'button-group' },
+            { elem: 'title' },
+            { elem: 'text' },
+            { elem: 'label' },
+            { elem: 'submit' },
+            { elem: 'text-error' },
+            { elem: 'head-title' },
+            { block: 'checkbox' },
+            { block: 'menu', mods: { theme: 'islands' }, tech: 'css' },
+            { block: 'select', mods: { theme: 'islands' }, tech: 'css' },
+            { block: 'input', mods: { theme: 'islands' }, tech: 'css' }
+        ]
+    }
+])
